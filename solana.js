9872656fd0ed3a2e49c85a85a2ef37ae81ba7a53
Atomico592 <atomico592@gmail.com>
2025-08-18 12:45:35 +0600

    Аdd receiving links

--- solana.js
+++ solana.js
@@ -1,12 +1,12 @@
 import {Connection, Keypair, PublicKey, VersionedTransaction} from "@solana/web3.js";
 import bs58 from 'bs58';
-import 'dotenv/config';
 import axios from "axios";
+import 'dotenv/config';
 
 const USDT_ADRES = new PublicKey('Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB');
 const SOL_ADRES = new PublicKey('So11111111111111111111111111111111111111112');
 
-const connection = new Connection(process.env.RPC_URL, { commitment: 'confirmed' });
+const connection = new Connection(process.env.RPC_URL, { wsEndpoint: null,commitment: 'confirmed' });
 
 const wallet = Keypair.fromSecretKey(bs58.decode(process.env.WALLET_KEY));
 
@@ -27,7 +27,11 @@ const  getQuote = async ({ inputMint, outputMint, amount }) => {
                 slippageBps: 50,
                 amount,
                 restrictIntermediateTokens: true
-            }
+            },
+            headers: {
+                'Accept': 'application/json'
+            },
+            maxBodyLength: Infinity,
         });
         return res.data;
     } catch (e) {
@@ -37,8 +41,9 @@ const  getQuote = async ({ inputMint, outputMint, amount }) => {
 
 const buildSwapTx = async ({ quoteResponse }) => {
     const body = {
-        quoteResponse,
         userPublicKey: wallet.publicKey.toBase58(),
+        quoteResponse,
+        wrapUnwrapSOL: true,
         dynamicComputeUnitLimit: true,
         prioritizationFeeLamports: {
             priorityLevelWithMaxLamports: {
@@ -47,44 +52,47 @@ const buildSwapTx = async ({ quoteResponse }) => {
                 priorityLevel: 'veryHigh',
             },
         },
+
     };
 
     try {
         const res = await axios.post(
             'https://lite-api.jup.ag/swap/v1/swap',
             body,
-            { headers: { 'Content-Type': 'application/json' } }
+            { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }, maxBodyLength: Infinity }
         );
         return res.data.swapTransaction;
     } catch (e) {
         console.error(e.response?.data || e.message);
+        throw e;
     }
 };
+
 
 const signSendConfirm = async (base64Tx) => {
     const tx = VersionedTransaction.deserialize(Buffer.from(base64Tx, 'base64'));
     tx.sign([wallet]);
     const raw = tx.serialize();
     const signature = await connection.sendRawTransaction(raw, { skipPreflight: true, maxRetries: 2 });
-    const conf = await connection.confirmTransaction({ signature }, 'finalized');
-    if (conf.value.err) {
-        throw new Error(`Tx failed: ${JSON.stringify(conf.value.err)}\nhttps://solscan.io/tx/${signature}`);
-    }
-    return signature;
+    // const conf = await connection.confirmTransaction({ signature }, 'finalized');   // подтверждение tx
+    // if (conf.value.err) {
+    //     console.log(conf.value.err);
+    //     throw new Error(`Tx failed: ${JSON.stringify(conf.value.err)}\nhttps://solscan.io/tx/${signature}`);
+    // } else {
+    // }
+        return `https://solscan.io/tx/${signature}`
 }
 
 const currencyExchange = async ({inputMint, outputMint, humanAmount, decimal}) => {
     const amount = toAtomic(humanAmount, decimal);
     const quoteResponse = await getQuote({inputMint, outputMint, amount});
-    console.log(quoteResponse)
     if (!quoteResponse) return console.error('Не удалось получить квоту');
-
     const swapTX = await buildSwapTx({quoteResponse});
     const signature = await signSendConfirm(swapTX);
     return signature;
 }
 
-const test = async () => {
+const runTransaction = async () => {
 
     // 1) 1 USDT -> SOL
     const sig1 = await currencyExchange({
@@ -95,16 +103,16 @@ const test = async () => {
     });
 
     // 2) 0.01 SOL -> USDT
-    const sig2 = await swapOneDirection({
-        fromMint: SOL_ADRES,
-        toMint: USDT_ADRES,
-        humanAmount: 0.1,
-        decimals: 9,
-    });
+    // const sig2 = await currencyExchange({
+    //     inputMint: SOL_ADRES,
+    //     outputMint: USDT_ADRES,
+    //     humanAmount: 1,
+    //     decimals: 9,
+    // });
 
     console.log(sig1);
-    console.log(sig2);
+    // console.log(sig2);
 }
 
-test()
+runTransaction();
 
